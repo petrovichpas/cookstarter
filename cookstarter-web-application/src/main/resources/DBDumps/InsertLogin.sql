5cde53c5f20b1856ca741b48b7886367d949286a
PetrNikitin <zexa91x0@mail.ru>
2020-10-10 20:58:03 +0500

    for testing Pull request

---
+++ cookstarter-web-application/src/main/resources/DBDumps/InsertLogin.sql
@@ -0,0 +1,2 @@
+INSERT INTO cookstarterapp.logins (login, password, firstname, lastname, comment)
+    VALUES ('user', '$2y$12$RwJcgigHCNlqZLepp/Mm5OXDHkWqhrqFKnIDdL17.R3o9iXXY3iGC', 'Jon', 'Dow', 'User');
