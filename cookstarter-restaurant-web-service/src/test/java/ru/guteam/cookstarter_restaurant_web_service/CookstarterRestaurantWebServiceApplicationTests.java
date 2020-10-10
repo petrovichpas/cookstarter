7e5249d944f86cf2997b169d0c2a5026ef880b3b
Mikhail <vandaniel@mail.ru>
2020-10-10 22:12:45 +0300

    add cookstarter-restaurant-web-service module

---
+++ cookstarter-restaurant-web-service/src/test/java/ru/guteam/cookstarter_restaurant_web_service/CookstarterRestaurantWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.guteam.cookstarter_restaurant_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookstarterRestaurantWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
