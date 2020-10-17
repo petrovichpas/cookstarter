9722cf38ce10f3fadab0b335a4dc367314c20ce5
Cromag <alseko_2001@mail.ru>
2020-10-17 23:52:13 +0300

    add local jwt check, id not null check annotation, delete jwt check api

--- cookstarter-model/src/main/java/ru/guteam/cookstarter/api/enums/JwtCheckStatus.java
+++
@@ -1,6 +0,0 @@
-package ru.guteam.cookstarter.api.enums;
-
-public enum JwtCheckStatus {
-    ACTIVE,
-    MISSED
-}
--- cookstarter-order-service/src/main/java/ru/guteam/cookstarter/orderservice/service/AuthService.java
+++
@@ -1,6 +0,0 @@
-package ru.guteam.cookstarter.orderservice.service;
-
-public interface AuthService {
-
-    void checkToken(String token);
-}
