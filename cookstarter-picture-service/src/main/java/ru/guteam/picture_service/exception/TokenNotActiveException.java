dbd824113269296c485d00f6077c449ccac965f7
ParadiseRaider <ege_jc_god@mail.ru>
2020-10-24 14:17:01 +0400

    Сделал проверку токена внутри. Генерация имени картинки случайными символами

--- cookstarter-picture-service/src/main/java/ru/guteam/picture_service/exception/TokenNotActiveException.java
+++
@@ -1,7 +0,0 @@
-package ru.guteam.picture_service.exception;
-
-public class TokenNotActiveException extends RuntimeException {
-    public TokenNotActiveException(String message) {
-        super(message);
-    }
-}
